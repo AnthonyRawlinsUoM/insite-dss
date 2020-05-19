f866908de44992a80ce8a3b249a549db30dc32ba
Anthony Rawlins <anthony.rawlins@unimelb.edu.au>
2020-05-20 02:12:28 +1000

    numerics into SQLite test

--- monitor.js
+++ monitor.js
@@ -112,7 +112,7 @@ io.on('connection', (socket) => {
 
     let now = "";
 
-    let stmt = db.prepare(`INSERT into job VALUES(NULL, '${job.name}','${job.descr}','${job.uuid}','${job.submitter_name}','${now}','${job.submitter_email}',${job.weather_machine_kind},${job.fuel_machine_kind},${job.planburn_target_perc},${job.regsim_duration},${job.num_replicates},${job.harvesting_on})`);
+    let stmt = db.prepare(`INSERT into job VALUES(NULL, '${job.name}','${job.descr}','${job.uuid}','${job.submitter_name}','${now}','${job.submitter_email}',${job.weather_machine_kind},${job.fuel_machine_kind},${job.planburn_target_perc.valueOf()},${job.regsim_duration.valueOf()},${job.num_replicates.valueOf()},${job.harvesting_on})`);
 
     try {
       stmt.run(job);
